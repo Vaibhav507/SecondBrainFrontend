25c2d45b3468a1847fe941c826e262ce296d8289
unknown <vaibhavpratham507@gmail.com>
2025-01-10 17:20:48 +0530

    planning for new features

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -12,7 +12,8 @@ interface ModalProps {
 
 enum ContentType {
     Youtube = "youtube",
-    Twitter = "twitter"
+    Twitter = "twitter",
+    Notes = "notes"
 }
 
 export function CreateContentModal({ open, onClose }: ModalProps) {
@@ -43,20 +44,32 @@ export function CreateContentModal({ open, onClose }: ModalProps) {
     return (
         <div>
             {open && <div className="w-screen h-screen bg-black top-0 left-0 bg-gray-50 absolute flex justify-center z-10 items-center">
-                <div className="flex justify-center bg-white p-4 rounded-md">
+                <div className="flex justify-start items-center bg-white p-4 rounded-md w-[50%] h-[50%]">
                     <div className="flex flex-col gap-4">
                         <div className="flex justify-end cursor-pointer" onClick={onClose}>
                             <CloseIcon />
                         </div>
-                        <div className="flex flex-col gap-2">
-                            <Input reference={titleRef} placeholder="Title" />
-                            <Input reference={linkRef} placeholder="Link" />
+                        <div className="flex gap-4">
+                            <div className="flex flex-col gap-2">
+                                Title
+                                <Input reference={titleRef} placeholder="" />
+                            </div>
+
+                            <div className="flex flex-col gap-2">
+                                Link
+                                <Input reference={linkRef} placeholder="" />
+                            </div>
+                        </div>
+                        <div className="">
+                            Notes
+                            <textarea name="" id="" className="px-4 py-2 border w-full outline-gray-200 rounded-md"></textarea>
                         </div>
                         <div className="flex flex-col gap-2 justify-center">
                             <h1>Type</h1>
                             <div className="flex gap-2 justify-between items-center">
                                 <Button text="Youtube" variant={type === ContentType.Youtube ? "primary" : "secondary"} onClick={() => {setType(ContentType.Youtube)}} />
                                 <Button text="Twitter" variant={type === ContentType.Twitter ? "primary" : "secondary"} onClick={() => {setType(ContentType.Twitter)}} />
+                                <Button text="Notes" variant={type === ContentType.Notes ? "primary" : "secondary"} onClick={() => {setType(ContentType.Notes)}} />
                             </div>
                         </div>
                         <div className="flex justify-center">
